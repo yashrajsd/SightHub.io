11ee034c17ab9d2dbc50683687e9e065274688f7
Yashraj Deshmukh <yashrajdeshmukh@eng.rizvi.edu.in>
2025-01-08 15:53:26 +0530

    webhooks issue resolved hopefully

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -8,42 +8,43 @@ import { NextResponse } from "next/server";
 export async function POST(req:Request){
     const SIGNING_SECRET = process.env.WEBHOOK_SECRET
 
-    if(!SIGNING_SECRET){
-        throw new Error("Error: Please add SIGNING_SECRET you mf");
-    }
 
-    const wh = new Webhook(SIGNING_SECRET);
-
-    const headerPayload = await headers();
-    const svix_id = headerPayload.get('svix-id');
-    const svix_timestamp = headerPayload.get("svix-timestamp");
-    const svix_signature = headerPayload.get('svix-signature');
-
-    if(!svix_id || !svix_timestamp || !svix_signature){
-        return new Response('Error: Svix Chud gaya bhidu',{
-            status:400
+    if (!SIGNING_SECRET) {
+        throw new Error('Error: Please add SIGNING_SECRET from Clerk Dashboard to .env or .env.local')
+      }
+    
+      const wh = new Webhook(SIGNING_SECRET)
+    
+      const headerPayload = await headers()
+      const svix_id = headerPayload.get('svix-id')
+      const svix_timestamp = headerPayload.get('svix-timestamp')
+      const svix_signature = headerPayload.get('svix-signature')
+    
+      if (!svix_id || !svix_timestamp || !svix_signature) {
+        return new Response('Error: Missing Svix headers', {
+          status: 400,
         })
-    }
-
-    const payload = await req.json()
-    const body = JSON.stringify(payload)
-
-    let evt: WebhookEvent
-
-    try{
-        evt = wh.verify(body,{
-            'svix-id':svix_id,
-            'svix-signature':svix_signature,
-            'svix-timestamp':svix_timestamp
+      }
+    
+      const payload = await req.json()
+      const body = JSON.stringify(payload)
+    
+      let evt: WebhookEvent
+    
+      try {
+        evt = wh.verify(body, {
+          'svix-id': svix_id,
+          'svix-timestamp': svix_timestamp,
+          'svix-signature': svix_signature,
         }) as WebhookEvent
-    }catch(err){
-        console.error("Error: Webhook chud gaya guru: ",err)
-        return new Response("Error: Verifition ki ma ki akh ho gayi (error)",{
-            status:400
+      } catch (err) {
+        console.error('Error: Could not verify webhook:', err)
+        return new Response('Error: Verification error', {
+          status: 400,
         })
-    }
-    const { id } = evt.data
-    const eventType = evt.type
+      }
+      const { id } = evt.data
+      const eventType = evt.type
 
     if(eventType=='user.created'){
         const {id,email_addresses,first_name,last_name} = evt.data
