e5d886801eb1c2d83480328507c22747bca736c3
Yashraj Deshmukh <yashrajdeshmukh@eng.rizvi.edu.in>
2025-01-08 15:38:53 +0530

    middleware resolved for webhooks

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,17 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-const isProtectedRoute = createRouteMatcher(['/dashboard(.*)'])
+const isProtectedRoute = createRouteMatcher(['/dashboard(.*)']);
+const isPublicRoute = createRouteMatcher(['/api/webhooks/clerk']);
 
 export default clerkMiddleware(async (auth, req) => {
-  
+  if (isPublicRoute(req)) return;
   if (isProtectedRoute(req)) await auth.protect();
-})
-
-
+});
 
 export const config = {
   matcher: [
+    // Match all routes except those excluded in the middleware
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     '/(api|trpc)(.*)',
   ],
-}
+};
